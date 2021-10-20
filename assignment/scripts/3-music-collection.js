2d17392b63011cc2443d501b37973abcdc2093c1
Kelsey Brown <kbrown55347@gmail.com>
2021-10-20 14:48:47 -0500

    Created and tested addToCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,31 @@
 console.log('***** Music Collection *****')
+
+let collection = [];
+
+function addToCollection(title, artist, yearPub) {
+  let newAlbum = {
+    title: title,
+    artist: artist,
+    yearPub: yearPub
+  }
+  collection.push(newAlbum);
+  return newAlbum;
+}
+
+console.log('Added album to collection:', addToCollection('reputation', 'Taylor Swift', 2017));
+console.log('Added album to collection:', addToCollection('Fearless', 'Taylor Swift', 2008));
+console.log('Added album to collection:', addToCollection('Let It Be', 'The Beatles', 1968));
+console.log('Added album to collection:', addToCollection('We Sing. We Dance. We Steal Things.', 'Jason Mraz', 2008));
+console.log('Added album to collection:', addToCollection('Waiting for My Rocket to Come', 'Jason Mraz', 2002));
+console.log('Added album to collection:', addToCollection('No Strings Attached', 'NSYNC', 2000));
+
+console.log(collection);
+
+let showCollection([]) {
+  console.log(showCollection.length());
+  for (info of showCollection) {
+    return `${title} by ${artist}, published in ${yearPub}.`;
+  }
+}
+
+showCollection(collection);
