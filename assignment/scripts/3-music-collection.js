a3be0393228ddb37a5d3cddc158b816e23689e1e
Kelsey Brown <kbrown55347@gmail.com>
2021-10-21 14:56:29 -0500

    Removed tracks parameter & added question in html file

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -4,29 +4,22 @@ let collection = [];
 
 console.log(`******** In addToCollection ********`);
 
-function addToCollection(title, artist, yearPub, tracks) {
+function addToCollection(title, artist, yearPub) {
   let newAlbum = {
     title: title,
     artist: artist,
     yearPub: yearPub,
-    tracks: tracks,
   }
   collection.push(newAlbum);
   return newAlbum;
 }
 
-console.log('Album added:', addToCollection('reputation', 'Taylor Swift', 2017,
-['...Ready for It?: 3:28', 'End Game: 4:04', 'I Did Something Bad: 3:58']));
-console.log('Album added:', addToCollection('Fearless', 'Taylor Swift', 2008,
-['Fearless: 4:03', 'Fifteen: 4:55', 'Love Story: 3:55']));
-console.log('Album added:', addToCollection('Let It Be', 'The Beatles', 1968,
-['Two Of Us: 3:37', 'Dig A Pony: 3:55', 'Across The Universe: 3:48']));
-console.log('Album added:', addToCollection('We Sing. We Dance. We Steal Things.', 'Jason Mraz', 2008,
-['Make It Mine: 3:08', "I'm Yours: 4:03", 'Lucky: 3:10']));
-console.log('Album added:', addToCollection('Waiting for My Rocket to Come', 'Jason Mraz', 2002,
-['You and I Both: 3:39', "I'll Do Anything: 3:12", 'Who Needs Shelter: 3:12']));
-console.log('Album added:', addToCollection('No Strings Attached', 'NSYNC', 2000,
-['Bye Bye Bye: 3:20', "It's Gonna Be Me: 3:12", 'Space Cowboy: 4:23']));
+console.log('Album added:', addToCollection('reputation', 'Taylor Swift', 2017));
+console.log('Album added:', addToCollection('Fearless', 'Taylor Swift', 2008));
+console.log('Album added:', addToCollection('Let It Be', 'The Beatles', 1968));
+console.log('Album added:', addToCollection('We Sing. We Dance. We Steal Things.', 'Jason Mraz', 2008));
+console.log('Album added:', addToCollection('Waiting for My Rocket to Come', 'Jason Mraz', 2002));
+console.log('Album added:', addToCollection('No Strings Attached', 'NSYNC', 2000));
 
 console.log('Check out my collection:', collection);
 
